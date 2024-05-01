281a6b49739a0148826a025615f6630edbd482e2
Antoine Mouchamps <antoine.mouchamps@gmail.com>
2024-05-01 15:46:58 +0200

    AR

--- analysis.py
+++ analysis.py
@@ -4,7 +4,7 @@ width = 0.025 # [m]
 
 analysis(path='in-depth',
          directory='results/hexagon-18',
-         file='10',
+         file='AR-0-10',
          U=10,
          time_interval=[1600, 1800],
          t_treshold=1250,
