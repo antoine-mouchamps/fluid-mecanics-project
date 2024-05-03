aeaf81885c3c8cc25157f23a77029048b9a07510
Antoine Mouchamps <antoine.mouchamps@gmail.com>
2024-05-03 11:53:31 +0200

    Results and charts part 1

--- analysis.py
+++ analysis.py
@@ -1,11 +1,90 @@
-from analysis_tools import analysis
+from analysis_tools import analysis, forces
 
 width = 0.025 # [m]
 
-analysis(path='in-depth',
-         directory='results/hexagon-18',
-         file='AR-0-10',
-         U=10,
-         time_interval=[1600, 1800],
-         t_treshold=1250,
-         )
+# analysis(path='in-depth',
+#          directory='results/hexagon-18',
+#          file='AR-0-10',
+#          U=10,
+#          t_treshold=1500,
+#          dt=10,
+#          )
+
+# analysis(path='in-depth',
+#          directory='results/hexagon-18',
+#          file='AR-0.25-10',
+#          U=10,
+#          t_treshold=1500,
+#          dt=10,
+#          )
+
+# analysis(path='in-depth',
+#          directory='results/hexagon-18',
+#          file='AR-0.5-10',
+#          U=10,
+#          t_treshold=1500,
+#          dt=10,
+#          )
+
+# analysis(path='in-depth',
+#          directory='results/hexagon-18',
+#          file='AR-1-10',
+#          U=10,
+#          t_treshold=1500,
+#          dt=10,
+#          )
+
+# analysis(path='in-depth',
+#          directory='results/hexagon-18',
+#          file='AR-0-100',
+#          U=100,
+#          t_treshold=250,
+#          dt=1,
+#          )
+
+# analysis(path='in-depth',
+#          directory='results/hexagon-18',
+#          file='AR-0.25-100',
+#          U=100,
+#          t_treshold=250,
+#          dt=1,
+#          )
+
+# analysis(path='in-depth',
+#          directory='results/hexagon-18',
+#          file='AR-0.5-100',
+#          U=100,
+#          t_treshold=250,
+#          dt=1,
+#          )
+
+
+# analysis(path='in-depth',
+#          directory='results/hexagon-18',
+#          file='AR-1-100',
+#          U=100,
+#          t_treshold=250,
+#          dt=1,
+#          )
+
+# analysis(path='in-depth',
+#          directory='results/hexagon-18',
+#          file='AR-0-1071',
+#          U=1071,
+#          t_treshold=25,
+#          dt=0.25,
+#          )
+
+# analysis(path='in-depth',
+#          directory='results/hexagon-18',
+#          file='AR-0-5000',
+#          U=5000,
+#          t_treshold=2,
+#          dt=0.005,
+#          )
+
+forces(Res=[205, 2053, 21992, 102669],
+       C_Ls_RMS=[],
+       C_Ds=[[1.301, 2.256, 0, 0], [0.070, 1.679, 0, 0], [0.932, 1.042, 0, 0], [0.822, 0.664, 0, 0]],
+       Sts =[[0.129, 0.120, 0, 0], [0.140, 0.120, 0, 0], [0.140, 0.140, 0, 0], [0.140, 0.180, 0, 0]]
+       )
--- analysis_tools.py
+++ analysis_tools.py
@@ -10,7 +10,7 @@ plt.rcParams.update({'font.size': 15})
 
 width = 0.025 # [m]
 
-def analysis(path, directory, file, U, time_interval, t_treshold, y_lims=[-5, 5], D=0.3, L=0.3):
+def analysis(path, directory, file, U, t_treshold, dt, y_lims=[-5, 5], D=0.3, L=0.3):
     Re = Reynolds(U/1000, D)
     print(f'Reynolds Number: {Re:.2f}')
     
@@ -29,22 +29,22 @@ def analysis(path, directory, file, U, time_interval, t_treshold, y_lims=[-5, 5]
                 raw_lifts.append(float(row[5])/width)
                 raw_drags.append(float(row[10])/width)
     
-    t1, t2 = time_interval
-    
     indices = [i for i in range(len(times)) if times[i] > t_treshold]
     
-    spectrum = fft.fft([raw_lifts[i] for i in indices])
-    freqs = fft.fftfreq(len(spectrum), d=10)
+    spectrum = fft.rfft([raw_lifts[i] for i in indices])
+    freqs = fft.rfftfreq(len(indices), d=dt)
     
-    freq = abs(freqs[[i for i in range(len(spectrum)) if spectrum[i] == max(spectrum)][0]])
+    freq = abs(freqs[[i for i in range(1, len(spectrum)) if round(abs(spectrum[i]),5) == round(max(abs(spectrum[1:])),5)][0]])
     
-    # fig, ax = plt.subplots()
-    # ax.plot(freq, abs(spectrum), label=r'$C_{d}$')
-    # ax.legend()
-    # # ax.set_xlabel(r'Temps $[s]$')
-    # # ax.set_ylabel(r'|Forces| $[mN/m]$')
+    fig, ax = plt.subplots()
+    ax.semilogy(freqs, abs(spectrum), label=r'$C_{d}$')
+    ax.legend()
+    ax.set_xlabel(r'Frequency')
+    ax.set_ylabel(r'|A|')
     
-    # fig.savefig(f'{path}/FFT-{file}-{round(Re)}.pdf', bbox_inches='tight')
+    ax.grid()
+    
+    fig.savefig(f'{path}/FFT-{file}-{round(Re)}.pdf', bbox_inches='tight')
     
     # i_max1 = [i for i in range(len(times)) if times[i] > t1-t1/10 and times[i] <= t1+t1/10][0]
     # i_max2 = [i for i in range(len(times)) if times[i] > t2-t2/10 and times[i] <= t2+t2/10][0]
@@ -53,8 +53,8 @@ def analysis(path, directory, file, U, time_interval, t_treshold, y_lims=[-5, 5]
 
     # freq = 1/(times[i_max2] - times[i_max1])
     
-    print(f'frequency: {freq:.1E} Hz')
-    print(f'Strouhal Number: {Strouhal(U/1000, D, freq):.2f}')
+    print(f'frequency: {freq:.2E} Hz')
+    print(f'Strouhal Number: {Strouhal(U/1000, D, freq):.3f}')
 
     # lifts = [raw_lifts[i] for i in range(i_max1, i_max2+1)]
     # drags = [raw_drags[i] for i in range(i_max1, i_max2+1)]
@@ -69,9 +69,11 @@ def analysis(path, directory, file, U, time_interval, t_treshold, y_lims=[-5, 5]
     
     C_Ls = [C_L(lift/1000, U/1000, L) for lift in raw_lifts]
     C_Ds = [C_D(drag/1000, U/1000, D) for drag in raw_drags]
+    
+    print(f'C_drag: {C_Ds[-1]:.3f}')
 
-    ax.plot(times, C_Ls, label=r'$C_{d}$')
-    ax.plot(times, C_Ds, label=r'$C_l$')
+    ax.plot(times, C_Ls, label=r'$C_{\ell}$')
+    ax.plot(times, C_Ds, label=r'$C_{d}$')
 
     ax.legend()
 
@@ -86,3 +88,24 @@ def analysis(path, directory, file, U, time_interval, t_treshold, y_lims=[-5, 5]
     fig.savefig(f'{path}/{file}-{round(Re)}.pdf', bbox_inches='tight')
 
     fileee.close()
+    
+    print('\n')
+    
+def forces(Res, C_Ls_RMS, C_Ds, Sts):
+    ax:Axes = None
+    fig, ax = plt.subplots()
+    
+    ARs = [0, 0.25, 0.5, 1]
+    
+    for C_D, AR in zip(C_Ds, ARs):
+        ax.semilogx(Res, C_L, label=r'$C_{{d}} pour AR={}$'.format(AR))
+
+    ax.legend()
+
+    ax.set_xlabel(r'Re')
+    ax.set_ylabel(r'C_{{d}}')
+    
+    ax.grid()
+
+    ax.grid(True)
+    fig.savefig(f'lifts_evolution.pdf', bbox_inches='tight')
